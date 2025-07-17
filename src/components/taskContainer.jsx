e892d3bf14046464e716ff35e2ec2c88e507a025
Rayane-HASSAINE <hassaine.ray@gmail.com>
2025-07-17 17:49:31 +0200

    ajout du e.preventDefault permettant de ne plus actualiser la page a chaque ajo ut de tache. Ajout d'une boucle if permettant de generer un message d'erreur sur la console 'taskTitle is empty afin d'eviter les abus de tache vide par l'utilisateur (a completer via un ME sur l'interface)

--- src/components/taskContainer.jsx
+++ src/components/taskContainer.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './header/header.jsx'
 import TaskInput from './taskInput/taskInput.jsx'
 import TaskList from './taskList/TaskList.jsx'
 import Footer from './footer/footer.jsx'
 
 const TaskContainer = () => {
-  return (
 
+  const [tasksList, setTasksList] = useState([
+    // {
+    //   id: 1,
+    //   title: "Faire la vaisselle",
+    //   completed: false,
+    // },
+
+    // {
+    //   id: 2,
+    //   title: "Sortir le chien",
+    //   completed: true,
+    // }
+  ]);
+
+    const addTask = (title) => {
+      const newTask = {
+        id: 5,
+        title: title,
+        completed: false,
+      }
+      setTasksList([...tasksList, newTask])
+    }
+
+  console.log(tasksList);
+
+  return (
     <main>
-        <Header/>
-        <TaskInput/>
-        <TaskList />
-        <Footer />
+      <Header />
+      <TaskInput addTask={addTask} />
+      <TaskList />
+      <Footer />
     </main>
   )
 }
--- src/components/taskInput/taskInput.jsx
+++ src/components/taskInput/taskInput.jsx
@@ -1,20 +1,47 @@
+import { useState } from 'react';
 import styles from './TaskInput.module.css'
 
-const TaskInput = () => {
-    return (
+const TaskInput = ({ addTask }) => {
 
-        <div className={`box ${styles.element}`}>
-            <h2 className={styles.title}>
-                Ajoute la prochaine tâche
-            </h2>
-            <form className={styles.container}>
-                <input type="text" className={styles.input} placeholder='Indiquez un titre de tache explicite.' />
-                <button className='button-primary' type="submit">
-                    Ajouter
-                </button>
-            </form>
-        </div>
-    )
+    const [taskTitle, setTaskTitle] = useState("");
+
+    const handleInputChange = (e) => {
+        setTaskTitle(e.target.value);
+    };
+
+    const handleAddtask = (e) => {
+        e.preventDefault();
+        if (taskTitle.trim()) {
+            addTask(taskTitle);
+            setTaskTitle("");
+        }
+        else {
+            console.log("taskTitle is empty");
+        }
+    };
+
+
+
+return (
+
+    <div className={`box ${styles.element}`}>
+        <h2 className={styles.title}>
+            Ajoute la prochaine tâche
+        </h2>
+        <form className={styles.container} onSubmit={handleAddtask}>
+            <input type="text"
+                className={styles.input}
+                placeholder='Indiquez un titre de tache explicite.'
+                onChange={handleInputChange}
+                value={taskTitle}
+            />
+
+            <button className='button-primary' type="submit">
+                Ajouter
+            </button>
+        </form>
+    </div>
+)
 }
 
 export default TaskInput
